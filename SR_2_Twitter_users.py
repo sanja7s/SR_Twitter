939aafa484fd77872855dc3e0376298e2cbdec92
sanja7s <sanjascepanovic@gmail.com>
2016-02-03 19:23:25 +0200

    organizing project structure'

--- SR_2_Twitter_users.py
+++ SR_2_Twitter_users.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from nltk.stem.snowball import SnowballStemmer
 from collections import defaultdict, OrderedDict
 import math
+import codecs
 
 STEMMER = SnowballStemmer("english", ignore_stopwords=True )
 
@@ -10,12 +11,17 @@ CV = ""
 AID7s = ""
 USR_TWEETS = ""
 
+#f_in = "usrs_with_more_than_20_tweets_COPY.dat"
+f_in = "usrs_with_more_than_20_tweets_100K_COPY.dat"
+f_out = "all_usr_pairs_SR_FASTEST_100K.dat"
+
+
 #############################################################################################
 # take the right collection = TF-IDF based concept vectors (CV) based on Wiki
 # and also the usr_clean_tweets collection
 #############################################################################################
 def set_global_conn_params(client = MongoClient(), dbs="test", CV_collection="CV_stemmed_pruned",
-	aid_collection="AID", usr_tweets_collection = "usr_clean_tweets_1K"):
+	aid_collection="AID", usr_tweets_collection = "usr_clean_tweets"):
 	global CV, AID7s, USR_TWEETS
 	# connect to Mongo db test
 	db = client[dbs]
@@ -50,14 +56,14 @@ def cosine_2_vectors(v1, v2):
 	v1_sq_sum = 0.0
 	v2_sq_sum = 0.0
 	keys_1 = set(v1.keys())
-	print len(keys_1)
+	#print len(keys_1)
 	keys_2 = set(v2.keys())
-	print len(keys_2)
+	#print len(keys_2)
 	# separate the different keys and common keys
 	different2_keys = keys_2 - keys_1
 	different1_keys = keys_1 - keys_2
 	common_keys = keys_1 & keys_2
-	print len(common_keys), common_keys
+	#print len(common_keys), common_keys
 	# for common keys, we calculate formula as is
 	# SR = v1.v2 / ||v1|| ||v2||
 	for term in common_keys:
@@ -178,6 +184,7 @@ def print_topN_text_concepts(txt, topN):
 		print_top_concept(v)
 
 
+# this function outputs a dictionary with {word: fq} pairs
 def stem_text_corpus(txt):
 	stemmed_text =  defaultdict(int)
 	for el in txt:
@@ -186,7 +193,7 @@ def stem_text_corpus(txt):
 			stemmed_text[stemmed_word] += int(fq)
 	return stemmed_text
 
-# txt_fq_dist is here given as a dict with word: fq elements
+# txt_fq_dist is here given as a dict with {word: fq} elements
 def extract_text_CV(txt_fq_dist):
 	CV_txt = defaultdict(int)
 	cv_word = defaultdict(int)
@@ -195,8 +202,8 @@ def extract_text_CV(txt_fq_dist):
 		if not cv_word:
 			continue
 		for concept, tf_idf in cv_word.iteritems():
-			if fq > 4:
-				print concept, tf_idf, fq
+			#if fq > 4:
+				#print concept, tf_idf, fq
 			CV_txt[concept] += tf_idf * fq
 	# print len(CV_txt.items())
 	return OrderedDict(sorted(CV_txt.items(), key=lambda x: x[1], reverse= False)) #normalize ? 
@@ -221,10 +228,105 @@ def SR_2_users(usrA, usrB):
 		print "No data found for user: ", usrB
 		return
 	txtB = B['txt']
-	print usrA, stem_text_corpus(txtA)
-	print usrB, stem_text_corpus(txtB)
+	#print usrA, stem_text_corpus(txtA)
+	#print usrB, stem_text_corpus(txtB)
 	return SR_2_texts(stem_text_corpus(txtA), stem_text_corpus(txtB))
 
+
+def read_users():
+	cnt_all_tweets = 0
+	users = defaultdict(int)
+
+	with codecs.open(f_in,'r', encoding='utf8') as input_file:
+		# the code loops through the input, collects each user into a dict
+	    for line in input_file:	
+	    	cnt_all_tweets += 1
+	    	line = line.split()
+	    	user = line[0]
+	    	users[user] = user
+	    	if cnt_all_tweets % 10000 == 0:
+	    		print user
+
+	print "Read: ", cnt_all_tweets, " tweets from: ", len(users.keys()), " distinct users."
+	return users
+
+
+# for a faster processing for all user pairs, we do not want to query MongoDB for the text 
+# more than once for one user; so here we colect all the texts in one big dictinary, since we
+# have enough memory for it. users is a dictionary representing a list of users.
+def extract_all_user_TXT(users):	
+	user_TXT_lst = defaultdict(int)
+
+	for userA in users.keys():
+		A = USR_TWEETS.find_one({"_id": userA})
+		txtA = A['txt']
+		user_TXT_lst[userA] = stem_text_corpus(txtA)
+	
+	return user_TXT_lst
+
+
+# TODO txt_fq_dist is here given as a dict with {word: fq} elements
+# make this faster and more memory demanding by readin in all words also
+# so that a concept for one word is never read in twice from MongoDB
+def extract_all_user_CVs(user_TXT_lst):
+	user_CVs_lst = defaultdict(int)
+	words_CVs_lst = defaultdict(int)
+
+	for userA in user_TXT_lst.iterkeys():
+		txt_fq_dist = user_TXT_lst[userA]
+		CV_txt = defaultdict(int)
+
+		for word, fq in txt_fq_dist.iteritems():
+			'''
+			# fancy code
+			cv_word = [words_CVs_lst[word] if words_CVs_lst[word] else extract_word_CV(word)]
+			words_CVs_lst[word] = cv_word
+			'''
+			# simple code
+			if words_CVs_lst[word]:
+				cv_word = words_CVs_lst[word]
+			else:
+				cv_word = extract_word_CV(word)
+				words_CVs_lst[word] = cv_word
+			
+			if not cv_word:
+				continue
+			for concept, tf_idf in cv_word.iteritems():
+				CV_txt[concept] += tf_idf * fq
+
+		user_CVs_lst[userA] = OrderedDict(sorted(CV_txt.items(), key=lambda x: x[1], reverse= False)) #normalize ? 
+
+	return user_CVs_lst
+
+
+# for all the users who are found to have more than threshold_tweets=20,
+# find their SR and save in a file
+def all_usr_SR():
+	# for the results
+	f = open(f_out, 'w')
+	SR_dict = defaultdict(int)
+
+	# a list of users with the threshold number of tweets found earlier
+	users = read_users()
+	# call a function to read in all the users' texts at once from MongoDB
+	user_TXT_lst = extract_all_user_TXT(users)
+	# call a function to extract CVs for all those user texts from the previous step
+	user_CVs_lst = extract_all_user_CVs(user_TXT_lst)
+
+	i = 0
+	for userA in users.iterkeys():
+		for userB in users.iterkeys():
+			CVA = user_CVs_lst[userA]
+			CVB = user_CVs_lst[userB]
+			SR = cosine_2_vectors(CVA, CVB)
+
+			#SR_dict[(userA,userB)] = SR
+			f.write(str(userA) + '\t' + str(userB) + '\t' + str(SR) + '\n')
+			i += 1
+			if i%1000 == 0:
+				print ":) we have ", i, " user pairs SR calculated"
+
+	return #SR_dict
 
 
 set_global_conn_params()
@@ -237,4 +339,6 @@ set_global_conn_params()
 
 #print_topN_user_concepts("rjwilson", 850)
 
-print_topN_common_user_concepts("rjwilson", "chowow")
+#print_topN_common_user_concepts("rjwilson", "chowow")
+
+all_usr_SR()
