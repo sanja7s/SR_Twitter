36505e171c7f26867be303ad7c74c4d8782ad8d4
sanja7s <sanjascepanovic@gmail.com>
2016-05-07 11:18:34 +0300

    just extract proper reciprocal and one side interaction networks

--- src_general/create_recip_weighted_network.py
+++ src_general/create_recip_weighted_network.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 IN_DIR = "../../../DATA/mention_graph/"
 f_weighted_edges_in = "mention_graph_weights.dat"
 f_recip_weighted_edges_out = "recip_mention_graph_weights.dat"
-
+f_recip_weighted_edges_out_simmetrical = "recip_mention_graph_weights_simmetrical.dat"
 
 def create_reciprocal_weighted():
 
@@ -37,4 +37,33 @@ def create_reciprocal_weighted():
 		w = recip_edges[(u1, u2)]
 		f_out.write(str(u1) + '\t' + str(u2) + '\t' + str(w) + '\n')
 
-create_reciprocal_weighted()
+def create_reciprocal_weighted_simmetrical():
+
+	os.chdir(IN_DIR)
+
+	f = open(f_weighted_edges_in, 'r') 
+	f_out = open(f_recip_weighted_edges_out_simmetrical, 'w') 
+
+	dir_edges = defaultdict(int) 
+
+	for line in f:
+		(uid1, uid2, w) = line.split()
+		uid1 = int(uid1)
+		uid2 = int(uid2)
+		w = int(w)
+		dir_edges[(uid1, uid2)] = w
+
+	recip_edges = defaultdict(int)
+	for edge in dir_edges:
+		u1, u2 = edge[0], edge[1]
+		if u1 < u2:
+			if (u2, u1) in dir_edges and (u1, u2) in dir_edges:
+				recip_edges[(u1, u2)] = dir_edges[(u1, u2)]
+				recip_edges[(u2, u1)] = dir_edges[(u2, u1)]
+
+	for (u1, u2) in recip_edges:
+		w = recip_edges[(u1, u2)]
+		f_out.write(str(u1) + '\t' + str(u2) + '\t' + str(w) + '\n')
+
+#create_reciprocal_weighted()
+create_reciprocal_weighted_simmetrical()
