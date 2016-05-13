005f83af1c41e55510154c2172fe0c0a0a1b074d
sanja7s <sanjascepanovic@gmail.com>
2016-05-13 16:34:06 +0300

    calculate the last month (8) MO SR

--- src_SR/monthly_SR_all_Twitter_users.py
+++ src_SR/monthly_SR_all_Twitter_users.py
@@ -13,7 +13,7 @@ from multiprocessing import Process, Manager
 from multiprocessing.managers import BaseManager, DictProxy
 import os, codecs
 
-MO = "7"
+MO = "8"
 
 # where we work
 IN_DIR = "../../../DATA/General/"
